e6f2721dd6e94f9ce9a31d9d11eff8382ad81502
Hanna Yakusevych <anna.yakusevich@icloud.com>
2019-02-09 22:36:27 +0300

    A special structure for lifecycle logging is made, all logs are rewritten

---
+++ RaccoonChat/LogMaker.swift
@@ -0,0 +1,27 @@
+//
+//  LogMaker.swift
+//  RaccoonChat
+//
+//  Created by Анна Якусевич on 09/02/2019.
+//  Copyright © 2019 Hanna Yakusevych. All rights reserved.
+//
+
+struct LogMaker {
+    
+    // Set the default value to 'false' to disable activity logs
+    static var isEnabled = true
+    
+    static func appStateChanges(in method: String = #function, from state1: String, to state2: String) {
+        if LogMaker.isEnabled {
+            print("AppDelegate: Application moved from \"\(state1)\" to \"\(state2)\": \(method)")
+        }
+    }
+    
+    static func viewStateChanges(in method: String = #function, message: String) {
+        if LogMaker.isEnabled {
+            print("ViewController: " + message)
+        }
+    }
+    
+}
+
--- RaccoonChat/ViewController.swift
+++ RaccoonChat/ViewController.swift
@@ -18,40 +18,34 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        printViewStateChanges(from: "Disappeared", to: "Appearing")
+        LogMaker.viewStateChanges(message: "View is appearing")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        printViewStateChanges(from: "Appearing", to: "Appeared")
+        LogMaker.viewStateChanges(message: "View appeared")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        printViewStateChanges(from: "-", to: "Changing Layout")
+        LogMaker.viewStateChanges(message: "Layout is changing")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        printViewStateChanges(from: "Changing Layout", to: "-")
+        LogMaker.viewStateChanges(message: "Layout changed")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        printViewStateChanges(from: "Appeared", to: "Disappearing")
+        LogMaker.viewStateChanges(message: "View is disappearing")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        printViewStateChanges(from: "Disappearing", to: "Disappeared")
+        LogMaker.viewStateChanges(message: "View disappeared")
+        
     }
-    
-    // MARK: Lifecycle logging methods
-    
-    private func printViewStateChanges(in method: String = #function, from state1: String, to state2: String) {
-        print("ViewController: Application moved from \"\(state1)\" to \"\(state2)\": \(method)")
-    }
-
 
 }
 
