b55ee3644cbd25917ecc81e6260158d83240139e
Hanna Yakusevych <anna.yakusevich@icloud.com>
2019-02-25 11:40:49 +0300

    The conversation list screen is added

---
+++ RaccoonChat/ConversationCell.swift
@@ -0,0 +1,127 @@
+//
+//  ConversationCell.swift
+//  RaccoonChat
+//
+//  Created by Анна Якусевич on 24/02/2019.
+//  Copyright © 2019 Hanna Yakusevych. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ConversationCell: UITableViewCell, ConversationCellConfiguration {
+  
+  // MARK: ConversationCellConfiguration conformance
+  var name: String? {
+    get {
+      return nameLabel.text
+    }
+    set {
+      nameLabel.text = newValue ?? "Name"
+    }
+  }
+  
+  var message: String? {
+    get {
+      return messageLabel.text
+    }
+    set {
+      if newValue == nil {
+        messageLabel.text = "No messages yet"
+        messageLabel.font = UIFont.italicSystemFont(ofSize: messageLabel.font.pointSize)
+      }
+      else {
+        messageLabel.text = newValue
+        messageLabel.font = UIFont.systemFont(ofSize: messageLabel.font.pointSize)
+      }
+    }
+  }
+  
+  var date: Date? {
+    get {
+      if dateLabel.text == nil {
+        return nil
+      }
+      let dateFormatter = DateFormatter()
+      return dateFormatter.date(from: dateLabel.text!)
+    }
+    set {
+      let dateFormatter = DateFormatter()
+      let calendar = Calendar.current
+      
+      if newValue == nil {
+        dateLabel.text = nil
+      }
+      else {
+        if calendar.isDateInToday(newValue!) {
+          dateFormatter.setLocalizedDateFormatFromTemplate("HH:mm")
+        }
+        else {
+          dateFormatter.setLocalizedDateFormatFromTemplate("dd MMM")
+        }
+        dateLabel.text = dateFormatter.string(from: newValue!)
+      }
+    }
+  }
+  
+  var online: Bool {
+    get {
+      return self.backgroundColor != #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    set {
+      self.backgroundColor = newValue ? #colorLiteral(red: 1, green: 0.9781315923, blue: 0.8520652652, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+  }
+  
+  var hasUnreadMessages: Bool {
+    get {
+      return messageLabel.font.fontDescriptor.symbolicTraits.contains(.traitBold)
+    }
+    set {
+      if newValue {
+        messageLabel.font = UIFont.boldSystemFont(ofSize: messageLabel.font.pointSize)
+      }
+      else {
+        messageLabel.font = UIFont.systemFont(ofSize: messageLabel.font.pointSize)
+      }
+    }
+  }
+  
+  var photo: UIImage? {
+    get {
+      return profileImageView.image
+    }
+    set {
+      profileImageView.image = newValue ?? UIImage(named: "placeholder-user")
+    }
+  }
+
+  // MARK: Outlets
+  
+  @IBOutlet var profileImageView: UIImageView!
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var dateLabel: UILabel!
+  @IBOutlet var messageLabel: UILabel!
+  
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+
+}
+
+protocol ConversationCellConfiguration : class {
+  var name: String? {get set}
+  var message: String? {get set}
+  var date: Date? {get set}
+  var online: Bool {get set}
+  var hasUnreadMessages: Bool {get set}
+  var photo: UIImage? {get set}
+}
---
+++ RaccoonChat/VIewControllers/ConversationsListViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ConversationsListViewController.swift
+//  RaccoonChat
+//
+//  Created by Анна Якусевич on 24/02/2019.
+//  Copyright © 2019 Hanna Yakusevych. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsListViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    tableView.reloadData()
+    // Do any additional setup after loading the view.
+  }
+  
+  // MARK: UITableViewDataSource methods
+  
+  // Just for representing cell styles
+  let onlineUsers = [User(name: "Name 1", message: nil, date: nil, online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 7", message: "Hello", date: Date(timeIntervalSince1970: 10000000), online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 2", message: "Hello", date: Date(timeIntervalSince1970: 500), online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 3", message: "Hello", date: Date(timeIntervalSinceNow: -30), online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 4", message: "Hello", date: Date(timeIntervalSinceNow: -900), online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 5", message: "Hello", date: Date(timeIntervalSince1970: 500), online: true, hasUnreadMessages: true, photo: nil),
+                     User(name: "Name 6", message: "Hello", date: Date(timeIntervalSinceNow: -30), online: true, hasUnreadMessages: true, photo: nil),
+                     User(name: "Name 8", message: "Hello", date: Date(timeIntervalSinceNow: -30000), online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 9", message: "Hello", date: Date(timeIntervalSince1970: 500), online: true, hasUnreadMessages: false, photo: nil),
+                     User(name: "Name 10", message: "Hello", date: Date(timeIntervalSinceNow: -90), online: true, hasUnreadMessages: false, photo: nil)]
+  
+  let offlineUsers = [User(name: "Name 11", message: nil, date: nil, online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 12", message: "Hello", date: Date(timeIntervalSince1970: 500), online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 13", message: "Hello", date: Date(timeIntervalSinceNow: -30), online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 14", message: "Hello", date: Date(timeIntervalSinceNow: -30), online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 15", message: "Hello", date: Date(timeIntervalSinceNow: -30), online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 16", message: "Hello", date: Date(timeIntervalSince1970: 500), online: false, hasUnreadMessages: true, photo: nil),
+                      User(name: "Name 17", message: "Hello", date: Date(timeIntervalSinceNow: -300), online: false, hasUnreadMessages: true, photo: nil),
+                      User(name: "Name 18", message: "Hello", date: Date(timeIntervalSinceNow: -30), online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 19", message: "Hello", date: Date(timeIntervalSince1970: 5000000), online: false, hasUnreadMessages: false, photo: nil),
+                      User(name: "Name 20", message: "Hello", date: Date(timeIntervalSinceNow: -3000000), online: false, hasUnreadMessages: false, photo: nil)]
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ConversationCell", for: indexPath) as! ConversationCell
+    if indexPath.section == 0 {
+      cell.name = onlineUsers[indexPath.row].name
+      cell.message = onlineUsers[indexPath.row].message
+      cell.date = onlineUsers[indexPath.row].date
+      cell.online = onlineUsers[indexPath.row].online
+      cell.hasUnreadMessages = onlineUsers[indexPath.row].hasUnreadMessages
+      cell.photo = onlineUsers[indexPath.row].photo
+    }
+    else {
+      cell.name = offlineUsers[indexPath.row].name
+      cell.message = offlineUsers[indexPath.row].message
+      cell.date = offlineUsers[indexPath.row].date
+      cell.online = offlineUsers[indexPath.row].online
+      cell.hasUnreadMessages = offlineUsers[indexPath.row].hasUnreadMessages
+      cell.photo = offlineUsers[indexPath.row].photo
+    }
+    
+
+    return cell
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    if section == 0 {
+      return "Online"
+    }
+    return "History"
+  }
+  
+  // MARK: UITableViewDelegate methods
+  
+    
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      // Get the new view controller using segue.destination.
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
+
+// MARK: User class - just for the task
+struct User {
+  var name = "Name"
+  var message: String? = nil
+  var date: Date? = nil
+  var online = false
+  var hasUnreadMessages = false
+  var photo: UIImage? = nil
+}
