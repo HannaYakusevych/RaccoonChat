20af31f36eb278e7931cfa30ad17a1a021da5399
Hanna Yakusevych <anna.yakusevich@icloud.com>
2019-03-14 00:12:05 +0300

    Git bug #2

--- RaccoonChat/SavingData/ProfileDataManager.swift
+++ RaccoonChat/SavingData/ProfileDataManager.swift
@@ -8,67 +8,8 @@
 
 import Foundation
 
-struct ProfileDataManager {
-  
-  static var user = AppUser()
-  static let imagePath = "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])/profilePhoto.jpg"
-  static let dataPath = "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])/profileData.txt"
-  
-  
-  static func saveData(name: String, description: String?, image: UIImage?) -> Bool {
-    // Image saving
-    if let image = image {
-      let imageUrl = URL(fileURLWithPath: imagePath)
-      do {
-        try image.jpegData(compressionQuality: 0.8)?.write(to: imageUrl)
-      } catch {
-        Logger.write("Image saving error")
-        return false
-      }
-    }
-    
-    // Data saving
-    user = AppUser(name: name, description: description)
-    let jsonEncoder = JSONEncoder()
-    do {
-      let jsonData = try jsonEncoder.encode(user)
-      let dataUrl = URL(fileURLWithPath: dataPath)
-      do {
-        try jsonData.write(to: dataUrl)
-      } catch {
-        Logger.write("Data saving error: unable to save data")
-        return false
-      }
-    } catch {
-      Logger.write("Data saving error: unable to encode the data")
-      return false
-    }
-    return true
-  }
-  
-  static func loadData() -> (name: String, description: String?, image: UIImage?) {
-    let imageUrl = URL(fileURLWithPath: imagePath)
-    
-    let jsonDecoder = JSONDecoder()
-    let dataUrl = URL(fileURLWithPath: dataPath)
-    if let jsonData = try? Data(contentsOf: dataUrl) {
-      
-      user = try! jsonDecoder.decode(AppUser.self, from: jsonData)
-    } else {
-      user = AppUser()
-    }
-    
-    guard FileManager.default.fileExists(atPath: imagePath), let imageData = try? Data(contentsOf: imageUrl), let image = UIImage(data: imageData, scale: UIScreen.main.scale) else {
-      print("Hello")
-      return (user.name, user.description, nil)
-    }
-    return (user.name, user.description, image)
-    
-  }
+protocol ProfileDataManager {
+  func saveProfileData(name:String?, description: String?, image: UIImage?, isSaved: @escaping (_ hasError: Bool) -> Void)
+  func loadProfileData(isDone: @escaping (Bool, (String, String, UIImage)) -> Void)
 }
 
-struct AppUser: Codable {
-  var name: String = ""
-  var description: String? = nil
-  //var image: URL?
-}
